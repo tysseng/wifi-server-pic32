ef0de056ed594e857f95bff3071a3008f4690531
unknown <Joakim@Gigasort.(none)>
2015-01-30 08:07:22 +0100

    working matrix with several nodes

--- OMM/OMM.c
+++ OMM/OMM.c
@@ -13,7 +13,10 @@ typedef struct matrixNode{
     // Bitwise variable that indicates usage of params:
     // 1 signifies that param is a constant
     // 0 that it is the index of the Node to get result from
-    short paramIsConstant;
+    unsigned short paramIsConstant;
+
+    // How many of the input params are used
+    unsigned short paramsInUse;
 
     // function to run when this Node is accessed
     nodeFunction func;
@@ -49,15 +52,64 @@ void interrupt(){
     //reset timer etc
 } */
 
+// params can be pointers to the result of the previous Node in the matrix or 
+// they can be constants. This function figures out which one and returns its 
+// value.
+unsigned short getParam(Node *aNode, unsigned short paramId){
+  unsigned short type;
+
+  type = (aNode->paramIsConstant >> paramId) & 0b00000001;
+  if(type == 1){
+    return aNode->params[paramId];
+  } else {
+    return nodes[aNode->params[paramId]]->result;
+  }
+}
+
+// sum an arbitrary number of inputs.
+void nodeFuncSum(Node *aNode){
+  unsigned short i;
+  aNode->result = 0;
+  for(i=0; i<aNode->paramsInUse; i++){
+    aNode->result += getParam(aNode, i);
+  }
+}
+
+// accepts a single input and inverts it.
+void nodeFuncInvert(Node *aNode){
+  unsigned short param;
+  param = getParam(aNode, 0);
+  
+  //hack to allow whole range to be inverted. chops off top as 128 is not
+  //possible with an 8 bit signed variable
+  if(param == -128){
+    aNode->result = 127;
+  } else {
+    aNode->result = -param;
+  }
+}
+
+// accepts a single input and iverts it around the "center" (64 or -63) of the 
+// side of 0 that the input is: 1 becomes 127 but -1 becomes -126.
+// usefull for inverting envelopes without turning them negative.
+void nodeFuncInvertEachSide(Node *aNode){
+  unsigned short param;
+  param = getParam(aNode, 0);
+
+  if(param > 0){
+    aNode->result = 127-param;
+  } else {
+    aNode->result = -128-param;
+  }
+}
+
+// add Node to the matrix.
 void addNode(Node *aNode){
     nodes[nodesInUse] = aNode;
     nodesInUse++;
 }
 
-void nodeFuncSum(Node *aNode){
-    aNode->result = aNode->params[0] + aNode->params[1];
-}
-
+// loop over the matrix array once and calculate all results
 void runMatrix(){
   unsigned short i;
   Node *aNode;
@@ -67,8 +119,28 @@ void runMatrix(){
   }
 }
 
+//TODO: Does not work with -128 for some reason
+void printSignedShort(unsigned short row, unsigned short col, short in){
+  short rest;
+  if(in < 0){
+    Lcd_Chr(row, col, '-');
+    in = -in;
+  } else {
+    Lcd_Chr(row, col, ' ');
+  }
+
+  rest = in % 10;
+  Lcd_Chr(row, col+3, 48 + rest);
+  
+  rest = (in / 10) % 10;
+  Lcd_Chr(row, col+2, 48 + rest);
+  
+  rest = (in / 100) % 10;
+  Lcd_Chr(row, col+1, 48 + rest);
+}
+
 void main() {
-    Node aNode;
+    Node aNode, aNode2, aNode3;
 
     // DEBUG STUFF
     Lcd_Init();                        // Initialize Lcd
@@ -77,12 +149,32 @@ void main() {
     Lcd_Out(1,1,txt1);                 // Write text in first row
 
     aNode.func = &nodeFuncSum;
-    aNode.params[0] = 4;
-    aNode.params[1] = 8;
+    aNode.params[0] = 120;
+    aNode.params[1] = 2;
+    aNode.params[2] = 1;
+    aNode.paramsInUse = 3;
+    aNode.paramIsConstant = 0b00000111;
     addNode(&aNode);
+
+    aNode2.func = &nodeFuncSum;
+    aNode2.params[0] = 1;
+    aNode2.params[1] = 1;
+    aNode2.paramsInUse = 2;
+    aNode2.paramIsConstant = 0b00000011;
+    addNode(&aNode2);
+
+    aNode3.func = &nodeFuncInvert;
+    aNode3.params[0] = 0;
+    aNode3.paramsInUse = 1;
+    aNode3.paramIsConstant = 0b00000000;
+    addNode(&aNode3);
 
     runMatrix();
 
     // DEBUG STUFF
-    Lcd_Chr(2,1,48+aNode.result);
+    printSignedShort(2,7,aNode.result);
+    Lcd_Chr(2,3,48+aNode2.result);
+    Lcd_Chr(2,5,48+aNode3.result);
+    
+    printSignedShort(2,7,-128);
 }
