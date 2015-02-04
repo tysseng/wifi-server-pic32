46668e75b6cd1b28d060859002327c0311104289
Joakim Tysseng <joakim.tysseng@sparebank1.no>
2015-02-04 17:34:53 +0100

    Added timer for dac and ramps. added binary functions and memory node, working on generalizing integer type for matrix

--- OMM/OMM.c
+++ OMM/OMM.c
@@ -1,10 +1,21 @@
 #define MAX_OPERATIONS 20
+#define MAX_SH_OUTPUTS 8
 
 #define UP 1
 #define DOWN 0
 
 #define RUNNING 1
 #define STOPPED 0
+
+//binary values. Anything from 0 and down is treated as false
+#define BINARY_TRUE 127
+#define BINARY_FALSE 0
+
+// datatype to use for matrix, makes it easier to switch between 8 and 16 bit
+// operations later.
+#define matrixint short
+#define MAX_POSITIVE 127
+#define MAX_NEGATIVE -128
 
 #include <built_in.h>
 
@@ -14,10 +25,10 @@ typedef void (*nodeFunction)(struct matrixNode *);
 //node in matrix
 typedef struct matrixNode{
   // placeholder for result from Node function
-    short result;
+    matrixint result;
 
     // parameters passed to this node
-    short params[8];
+    matrixint params[8];
 
     // high res status variable for ramps etc, to reduce effects of rounding
     // errors.
@@ -40,12 +51,36 @@ typedef struct matrixNode{
 
 // matrix nodes
 Node *nodes[MAX_OPERATIONS];
-short nodesInUse = 0;
+unsigned short nodesInUse = 0;
 
-short inputBuffer[8];
-short outputBuffer[8];
+// The number of dac updates finished since last time the matrix were run.
+// This is checked before a new runMatrix is called as timing is done through
+// a timer interrupt that increments what dac to update.
+// NB: As dacs may have been updated multiple times since the matrix run started,
+// any ramp increments should be multiplied by this number to get correct timing.
+unsigned short dacUpdatesFinished;
 
-short temp;
+// what sample-and-hold output to update
+unsigned short shToUpdate;
+
+// true if a matrix run has been completed and data is ready to be copied to
+// the dac buffer before the next dac cycle.
+unsigned short outputBufferComplete;
+
+// dac update timing - interval between each dac update (= s&h acquisition time)
+unsigned short dacIntervalTimerStartH;
+unsigned short dacIntervalTimerStartL;
+
+// place where matrix reads inputs from
+matrixint inputBuffer[8];
+
+// place where matrix write outputs to
+matrixint outputBuffer[MAX_SH_OUTPUTS];
+
+// buffer that dac reads from. outputs should be copied here before dac starts
+// writing them out, to make it possible to write to output while simultaneously
+// calculating the next run.
+matrixint dacBuffer[MAX_SH_OUTPUTS];
 
 // Lcd module connections
 sbit LCD_RS at LATB2_bit;
@@ -66,24 +101,55 @@ char txt1[] = "OMM illustrator";
 char txt2[] = "EasyPIC7";
 
 
+void interrupt() {
+    unsigned short i;
 
+    if(PIR1.TMR1IF){
+        //restart timer
+        TMR1H = dacIntervalTimerStartH;
+        TMR1L = dacIntervalTimerStartL;
+        PIE1.TMR1IE = 1;
+        PIR1.TMR1IF = 0;
 
-/*
-void interrupt(){
-    //reset timer etc
-} */
+        // copies output buffer to dac buffer to make sure nothing changes
+        // while updating dacs.
+        if(shToUpdate == 0){
+            if(outputBufferComplete){
+                for(i = 0; i < MAX_SH_OUTPUTS; i++){
+                    dacBuffer[i] = outputBuffer[i];
+                }
+                outputBufferComplete = 0;
+            }
+
+            // signal that data has been copied and that next matrix calculation
+            // may start.
+            dacUpdatesFinished++;
+        }
+
+        // TODO: Write to dac here
+        // writeToDac(dacBuffer[shToUpdate]);
+
+        // step to next sample and hold output
+        shToUpdate++;
+        
+        // finished cycling through all outputs //TODO: timing!
+        if(shToUpdate == MAX_SH_OUTPUTS){
+            shToUpdate = 0;
+        }
+    }
+}
 
 // params can be pointers to the result of the previous Node in the matrix or
 // they can be constants. This function figures out which one and returns its
 // value.
-short getParam(Node *aNode, unsigned short paramId){
+matrixint getParam(Node *aNode, unsigned short paramId){
     unsigned short type;
 
     type = (aNode->paramIsConstant >> paramId) & 0b00000001;
     if(type == 1){
         return aNode->params[paramId];
     } else {
-        return nodes[aNode->params[paramId]]->result;
+        return nodes[aNode->params[paramId]]->result; //TODO will this work with a 16 bit param?
     }
 }
 
@@ -92,20 +158,28 @@ void nodeFuncSum(Node *aNode){
     unsigned short i;
     aNode->result = 0;
     for(i=0; i<aNode->paramsInUse; i++){
-      temp = getParam(aNode, i);
-        aNode->result += temp;
+        aNode->result += getParam(aNode, i);
+    }
+}
+
+// multiply an arbitrary number of inputs.
+void nodeFuncMultiply(Node *aNode){
+    unsigned short i;
+    aNode->result = 0;
+    for(i=0; i<aNode->paramsInUse; i++){
+        aNode->result *= getParam(aNode, i);
     }
 }
 
 // accepts a single input and inverts it.
 void nodeFuncInvert(Node *aNode){
-    short param;
+    matrixint param;
     param = getParam(aNode, 0);
 
     //hack to allow whole range to be inverted. chops off top as 128 is not
     //possible with an 8 bit signed variable.
-    if(param == -128){
-        aNode->result = 127;
+    if(param == MAX_NEGATIVE){
+        aNode->result = MAX_POSITIVE;
     } else {
         aNode->result = -param;
     }
@@ -115,18 +189,18 @@ void nodeFuncInvert(Node *aNode){
 // side of 0 that the input is: 1 becomes 127 but -1 becomes -126.
 // usefull for inverting envelopes without turning them negative.
 void nodeFuncInvertEachSide(Node *aNode){
-    short param;
+    matrixint param;
     param = getParam(aNode, 0);
 
     if(param >= 0){
-        aNode->result = 127-param;
+        aNode->result = MAX_POSITIVE - param;
     } else {
-        aNode->result = -129-param;
+        aNode->result = MAX_NEGATIVE - 1 - param;
     }
 }
 
 // calculate increment necessary to get the requested ramp timing.
-int calculateRampIncrement(short setting, short direction, short bipolar){
+int calculateRampIncrement(matrixint setting, short direction, short bipolar){
     int highResSetting = setting;
     if(direction == 0){
         highResSetting = -highResSetting;
@@ -145,7 +219,7 @@ int calculateRampIncrement(short setting, short direction, short bipolar){
 void nodeFuncRamp(Node *aNode){
 
     int increment;
-    short trigger, startPosition;
+    matrixint trigger, startPosition;
     bit shouldResetWhenFinished, direction, bipolar;
 
     trigger = getParam(aNode, 1);
@@ -187,6 +261,50 @@ void nodeFuncDelayLine(Node *aNode){
     aNode->result = getParam(aNode, 0);
 }
 
+// memory with set and clear. 
+// Set if param 1 > 0, 
+// Clear if param 2 > 0 (resets to 0)
+void nodeFuncMemory(Node *aNode){
+    if(getParam(aNode, 2)){
+        aNode->result = 0;
+    } else if(getParam(aNode, 1)){
+        aNode->result = getParam(aNode, 0);
+    }
+}
+
+// treat input as a binary values and binary AND them
+void nodeFuncBinaryAnd(Node *aNode){
+    unsigned short paramNum;
+    aNode->result = BINARY_TRUE;
+    for(paramNum = 0; paramNum < aNode->paramsInUse; paramNum++){
+        if(getParam(aNode, paramNum) <= 0){
+            aNode->result = BINARY_FALSE;
+            break;
+        }
+    }
+}
+
+// treat input as a binary values and binary OR them
+void nodeFuncBinaryOr(Node *aNode){
+    unsigned short paramNum;
+    aNode->result = BINARY_FALSE;
+    for(paramNum = 0; paramNum < aNode->paramsInUse; paramNum++){
+        if(getParam(aNode, paramNum) > 0){
+            aNode->result = BINARY_TRUE;
+            break;
+        }
+    }
+}
+
+// treat input as a binary value and binary INVERT it
+void nodeFuncBinaryInvert(Node *aNode){
+    if(getParam(aNode,0) > 0){
+        aNode->result = BINARY_FALSE;
+    } else {
+        aNode->result = BINARY_TRUE;
+    }
+}
+
 // fetch  input from inputBuffer and add it as the result of a Node to be used
 // in the matrix
 void nodeFuncInput(Node *aNode){
@@ -212,6 +330,10 @@ void runMatrix(){
       aNode = nodes[i];
       aNode->func(aNode);
     }
+
+    // all nodes have written their data to the output buffer, tell
+    // dac loop that new data can be loaded when dac cycle restarts
+    outputBufferComplete = 1;
 }
 
 void printSignedShort(unsigned short row, unsigned short col, short in){
@@ -254,12 +376,44 @@ void writeToDac(){
      }
 }
 
+
+void dacTimerInit(){
+
+  // Enable GIE (all interrupt sources)
+  // Enable PEIE (all peripheral interrupt sources - timer1, 2, 3 etc)
+  // Enable T0IE (enable TMRO interrupt)
+  // Disable T0IF (clear TMR0 interrupt)
+  INTCON = 0xE0;
+
+  // Timer1 - DAC clock
+  TMR1H = dacIntervalTimerStartH;
+  TMR1L = dacIntervalTimerStartL;
+
+  T1CON = 0xA4; // Set to 16 bit, prescaler of 4 and timer stopped
+  PIR1.TMR1IF = 0; // clear timer 1 interrupt
+  PIE1.TMR1IE = 1; // enable timer 1 interrupt
+}
+
+// Start dac timer, should be run after everything else is ready to go to prevent
+// writing bogus data to outputs.
+void dacTimerStart(){
+  shToUpdate = 0;
+  T1CON.TMR1ON = 1;
+}
+
+void dacTimerStop(){
+  T1CON.TMR1ON = 0;
+}
+
+
 void main() {
     unsigned short iteration;
     Node aNode0, aNode1, aNode2, aNode3, aNode4, aNode5;
 
-    iteration =0;
-
+    iteration = 0;
+    dacUpdatesFinished = 0;             //necessary to start runMatrix.
+    dacTimerInit();
+    
     // DEBUG STUFF
     Lcd_Init();                        // Initialize Lcd
     Lcd_Cmd(_LCD_CLEAR);               // Clear display
@@ -298,13 +452,19 @@ void main() {
     aNode4.paramIsConstant = 0b00000000;
     addNode(&aNode4);
 
+    // calculate initial state. dacUpdatesFinished will be 0, so any ramps
+    // will not be incremented.
     runMatrix();
-    printSignedShort(2,1,outputBuffer[0]);
-    printSignedShort(2,12,iteration++);
+    
+    // start writing to outputs
+    dacTimerStart();
 
+    // tight loop that runs at most once for every dacUpdate cycle.
     while(1){
-        delay_ms(1000);
-        runMatrix();
+        if(dacUpdatesFinished){
+            dacUpdatesFinished = 0;
+            runMatrix();
+        }
         printSignedShort(2,1,outputBuffer[0]);
         printSignedShort(2,12,iteration++);
     }
