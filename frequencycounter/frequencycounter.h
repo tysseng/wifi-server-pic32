d76c3509c8c20906104922b5f699014ec83c5528
Joakim Tysseng <joakim@tysseng.no>
2018-01-27 14:40:55 +0100

    moved to separate repo

--- frequencycounter/frequencycounter.h
+++
@@ -1,8 +0,0 @@
-void interrupt(void);
-void main(void);
-void hardwareInit(void);          // setup hardware start conditions
-void frequencyCalculatorInit(unsigned short factor);
-void storeInput(void);            // copies a captured value into the array of periods.
-void startCapture();              // performs a capture of the selected number of pulses.
-unsigned int getAverageCapture();
-unsigned int calculateFrequency(unsigned int timerValue);
